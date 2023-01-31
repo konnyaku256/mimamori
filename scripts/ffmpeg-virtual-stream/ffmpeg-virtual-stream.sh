1e92ea8e742d5703165682aa65f8401be1d521d0
konnyaku256 <29503528+konnyaku256@users.noreply.github.com>
2023-01-31 16:03:47 +0900

    ビデオデバイスの多重化

--- scripts/ffmpeg-virtual-stream/ffmpeg-virtual-stream.sh
+++ scripts/ffmpeg-virtual-stream/ffmpeg-virtual-stream.sh
@@ -1,3 +1,5 @@
+sudo modprobe v4l2loopback video_nr=2,3
+
 ffmpeg \
   -f video4linux2 -input_format mjpeg -video_size 1280x720 -framerate 30 -i /dev/video0 \
   -f video4linux2 -vcodec copy /dev/video2 \
